4fcb0610d51d8834531d1fc94ba9698a4dae164a
lhe <lhe@bytecraft.com.au>
2015-08-28 14:34:16 +1000

    added ozlotto

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require_once dirname(__FILE__) . '/bootstrap.php';
 
 $dataFile = dirname(__FILE__) . '/data/ozlotto.db';
-NumberGenerator::generate(3, 2, $dataFile);
+NumberGenerator::generate(45, 7, $dataFile);
 if(!is_file($dataFile))
 	die('No data generated in: ' . $dataFile);
